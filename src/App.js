44e9959996ec9e543ae79821a84a062397504044
amrendrakind <vision2010@gmail.com>
2022-04-28 04:40:17 +0530

    Implemented reviewer's reccomendation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
-
 export default App;
