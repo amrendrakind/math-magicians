1ba2f0b476831a49d5073982ba37992e3aa26da5
amrendrakind <vision2010@gmail.com>
2022-05-05 18:13:44 +0530

    Add Quotes component

---
+++ src/pages/Quotes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Quotes() {
+  return (
+    <div>
+      <h3>Quotes of the Day!!!</h3>
+      <p>
+        Mathematics is not about numbers, computations, or diagrams. It is about
+        understanding. - William Paul Thurston.
+      </p>
+    </div>
+  );
+}
+export default Quotes;
