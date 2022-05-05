90badec0f8fe1cfa870b7a40e9be9a246b7085c4
amrendrakind <vision2010@gmail.com>
2022-05-05 16:29:20 +0530

    Add React router dom set-up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Calculator />
+      <Routes>
+        <Route path="/" component={<Home />} />
+        <Route path="/calculator" component={<Calculator />} />
+      </Routes>
     );
   }
 }
