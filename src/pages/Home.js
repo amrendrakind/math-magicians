a36e16ba28687f7eeab4baee137e1e59681015ba
amrendrakind <vision2010@gmail.com>
2022-05-05 22:20:41 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,30 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h3>Welcome to our page!</h3>
+    <div className="home-container">
+      <h2>Welcome to our page!</h2>
       <p>
-        Math Magicians Single Page Website is developed with React JS.
+        <b>Math Magicians Single Page Website is developed with React JS.</b>
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
       </p>
     </div>
   );
