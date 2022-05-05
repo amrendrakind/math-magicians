b9e040aebdcfe7acaa804bda877a0d5880880f0b
Oluyemi Paul <oluola96@gmail.com>
2022-05-05 15:54:15 -0600

    test for addition

--- src/tests/Operate.test.js
+++ src/tests/Operate.test.js
@@ -4,3 +4,12 @@ test('sum two values', () => {
   const result = operate(2, 3, '+');
   expect(result).toEqual('5');
 });
+test('substract one value from another', () => {
+  const result = operate(5, 2, '-');
+  expect(result).toEqual('3');
+});
+
+test('multiply two values', () => {
+  const result = operate(2, 3, 'x');
+  expect(result).toEqual('6');
+});
